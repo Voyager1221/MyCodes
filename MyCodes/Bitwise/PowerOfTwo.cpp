ed56e861b7597b14de723fae7f378fc0c4bc2786
Voyager1221 <88195679+Voyager1221@users.noreply.github.com>
2021-11-13 18:28:13 +0530

    Handled fornos less tha n1 for both power of three and four

--- MyCodes/Bitwise/PowerOfTwo.cpp
+++ MyCodes/Bitwise/PowerOfTwo.cpp
@@ -2,9 +2,19 @@
 
 using namespace std;
 
+bool powerOfTwo(int n){
+    if(n<1) return false;
+    while(n%2==0){
+        n = n/2;
+    }
+    return (n==1)?true:false;
+}
+
 int main(){
     int n;
     cin>>n;
+    //MY METHOD/ APPROACH
+/*
     if(n==1){
         cout<<"true"<<endl;
     }
@@ -15,5 +25,11 @@ int main(){
         cnt++;
     }
     int y = 1<<cnt-1;
+    
     cout<<y<<endl;
+*/
+    bool result = powerOfTwo(n);
+    cout<<"The result is : "<<result<<endl;
+    return 0;
+
 }
