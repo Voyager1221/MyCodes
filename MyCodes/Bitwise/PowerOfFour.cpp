7be4a7c904b1afc62181285f3821ac7305369370
Voyager1221 <88195679+Voyager1221@users.noreply.github.com>
2021-11-13 18:23:24 +0530

    Handled the nos which are less than 1

--- MyCodes/Bitwise/PowerOfFour.cpp
+++ MyCodes/Bitwise/PowerOfFour.cpp
@@ -3,11 +3,12 @@
 using namespace std;
 
 bool powerOfFour(int n){
+    if(n<1) return false;
     while(n%4==0){
         n = n/4;
     }
-    if(n==1) return 1;
-    else return 0;
+    if(n==1) return true;
+    else return false;
 }
 
 int main(){
