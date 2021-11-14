a0148cccda3b687b0e5be098cbc3d73b9134fe73
Voyager1221 <88195679+Voyager1221@users.noreply.github.com>
2021-11-15 02:31:40 +0530

    Using inheritance via importing

---
+++ MyCodes/Inheritance_and_Polymorphism/LivingThingKingdom/livingThing.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class livingThing{
+    void print();
+};
+
+void livingThing::print(){
+    cout<<"\nHey! This is livingThing's class"<<endl;
+    cout<<"\nLiving things are categorized into Animal Plants and Fungi"<<endl;
+}
+
+int main(){
+
+}
