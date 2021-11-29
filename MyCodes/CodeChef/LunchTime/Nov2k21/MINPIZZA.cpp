9eca0cc57951f5e79d76286d989b6503e2393fe1
Voyager1221 <88195679+Voyager1221@users.noreply.github.com>
2021-11-29 17:09:14 +0530

    MINPIZZA Solved

---
+++ MyCodes/CodeChef/LunchTime/Nov2k21/MINPIZZA.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+// int gcd(int a, int b){
+//     int GCD=1,m = min(a,b);
+//     for(int i=0; i<m; i++){
+//         if(a%m==0 && b%m==0){
+//             GCD = m;
+//         }
+//     }
+//     return GCD;
+// }
+
+int gcd(int a, int b){
+    if(b==0) return a;
+    return gcd(b, a%b);
+}
+
+void solve(){
+    int N,K;
+    cin>>N>>K;
+    cout<<N/gcd(N,K)<<endl;
+}
+
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
