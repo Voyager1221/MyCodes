b6c7d09c274fdc0e89a6ceb6d85fadc35051868c
Voyager1221 <88195679+Voyager1221@users.noreply.github.com>
2021-12-08 02:17:11 +0530

    3 questions solved on codeForces

--- MyCodes/SHUBHAM/CodeForces/9A.cpp
+++ MyCodes/SHUBHAM/CodeForces/9A.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(){
     int Y,W;
     cin>>Y>>W;
-    int numerator = 6 - max(Y,W) +1;
+    int numerator = 6 - max(Y,W) + 1;
     int GCF = __gcd(numerator, 6);
     cout<<numerator/GCF<<"/"<<6/GCF<<endl;
     return 0; 
