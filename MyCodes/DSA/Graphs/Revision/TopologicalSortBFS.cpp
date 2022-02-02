fc125ebc48d36ac6c85904774868dfc50d676691
Voyager1221 <aayush.bhadani111@gmail.com>
2022-02-02 17:04:50 +0530

    Topological Sort using BFS

---
+++ MyCodes/DSA/Graphs/Revision/TopologicalSortBFS.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+class TopologicalSort{
+    public:
+        void CreateAdjList(int M, vector<int>&in_deg, vector<vector<int>>&adjList);
+        void TopoSort(int N, vector<int>&in_deg, vector<int>&topo, vector<vector<int>>&adjList);
+        void Display(vector<int>&topo);
+};
+void TopologicalSort::CreateAdjList(int M, vector<int>&in_deg, vector<vector<int>>&adjList){
+    for(int i=0; i<M; i++){
+        int a,b;
+        cin>>a>>b;
+        adjList[a].push_back(b);
+        in_deg[b]++;
+    }
+}
+void TopologicalSort::TopoSort(int N, vector<int>&in_deg, vector<int>&topo, vector<vector<int>>&adjList){
+    queue<int>q;
+    
+    for(int i=1; i<=N; i++){
+        if(in_deg[i]==0){
+            q.push(i);
+        }
+    }
+
+    while(!q.empty()){
+        int node = q.front();
+        q.pop();
+        topo.push_back(node);
+        for(auto neigh: adjList[node]){
+            in_deg[neigh]--;
+            if(in_deg[neigh]==0){
+                q.push(neigh);
+            }
+        }
+    }
+}
+
+void TopologicalSort::Display(vector<int>&topo){
+    cout<<"\nTopological sort using BFS: ";
+    for(int i=0; i<topo.size(); i++){
+        cout<<topo[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+
+    int N, M;
+    cin>>N>>M;
+
+    vector<int>in_deg(N+1);
+    vector<int>topo;
+    vector<vector<int>>adjList(N+1);
+
+    TopologicalSort obj1;
+    obj1.CreateAdjList(M, in_deg, adjList);
+    obj1.TopoSort(N, in_deg, topo, adjList);
+    obj1.Display(topo);
+    return 0;
+}
