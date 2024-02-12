9f41a8ec105ac03ebe7c1c6f7d3a159b84820240
Voyager1221 <aayushbhadani111@gmail.com>
2024-02-12 21:03:44 +0530

    Solved the problem partially with the worst time complexity. Failed. Still Trying.

--- MyCodes/DSA/Recursion/RecursionRevision/NQueens.cpp
+++ MyCodes/DSA/Recursion/RecursionRevision/NQueens.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <typeinfo>
 
 using namespace std;
 
@@ -8,60 +9,85 @@ int smallerDifference(int a, int b){
     return a;
 }
 
-void nQueens(vector<vector<string>>&visited, vector<vector<int>>&dir, vector<string>&temp, int row, int col, int N){
+bool nQueens(vector<vector<string>>&visited, vector<vector<int>>&dir, int cnt, int row, int col, int N){
+       
+
     int p = row; int k = col;
-    visited[row][col] = 'Q';
 
     // Marking Top Left Diagonal Visited
     int topLeftRowDiff = row - 0;
     int topLeftColDiff = col - 0;
     int topLeftDiff = smallerDifference(topLeftRowDiff, topLeftColDiff);
     while(topLeftDiff--){
-        visited[--p][--k] = '.';
+        visited[--p][--k] = ".";
     }
 
     p = row; k = col;
     // Marking Top Right Diagonal Visited
     int topRightRowDiff = row - 0;
-    int topRightColDiff = N - col;
+    int topRightColDiff = (N-1) - col;
     int topRightDiff = smallerDifference(topRightRowDiff, topRightColDiff);
     while(topRightDiff--){
-        visited[--p][++k] = '.';
+        visited[--p][++k] = ".";
     }
 
     p = row; k = col;
     // Marking Bottom Left Diagonal Visited
-    int bottomLeftRowDiff = N - row;
+    int bottomLeftRowDiff = (N-1) - row;
     int bottomLeftColDiff = col - 0;
     int bottomLeftDiff = smallerDifference(bottomLeftRowDiff, bottomLeftColDiff);
     while(bottomLeftDiff--){
-        visited[++p][--k] = '.';
+        visited[++p][--k] = ".";
     }
 
     p = row; k = col;
     // Marking Bottom Right Diagonal Visited
-    int bottomRightRowDiff = N - row;
-    int bottomRightColDiff = N - col;
+    int bottomRightRowDiff = (N-1) - row;
+    int bottomRightColDiff = (N-1) - col;
     int bottomRightDiff = smallerDifference(bottomRightRowDiff, bottomRightColDiff);
     while(bottomRightDiff--){
-            visited[++p][++k] = '.';
+            visited[++p][++k] = ".";
     }
-
+    
     // Marking Current Row Visited
     for(int colItr=0; colItr<N; colItr++){
-        visited[row][colItr] = '.';
+        if(colItr != col){
+            visited[row][colItr] = ".";
+        }
     }
     // Marking Current Column Visited
     for(int rowItr=0; rowItr<N; rowItr++){
-        visited[rowItr][col] = '.';
+        if(rowItr != row){
+            visited[rowItr][col] = ".";
+        }
     }
+
+    visited[row][col] = "Q";
+    cnt++;
+
+    if(cnt == N)return true; 
 
     for(int itr = 0; itr<8; itr++){
         int di = row + dir[itr][0];
         int dj = col + dir[itr][1];
-        if((0<=di && di<N) && (0<=dj && dj<N) && visited[di][dj] == "0"){
-            nQueens(visited, dir, temp, di, dj, N);
+        if((0<=di && di<N) && (0<=dj && dj<N) && visited[di][dj] == "-1"){
+
+            return nQueens(visited, dir, cnt, di, dj, N);
         }
+    }
+    return false;
+}
+
+
+
+void display(vector<vector<string>>&visited){
+    cout<<"Printing the results: "<<endl;
+    for(int i=0; i<visited.size(); i++){
+        for(int j=0; j<visited[i].size(); j++){
+            cout<<visited[i][j]<<" ";
+            visited[i][j] = "-1";
+        }
+        cout<<endl;
     }
 }
 
@@ -71,17 +97,20 @@ int main(){
     cout<<"Enter the side of chessboard: "<<endl;
     cin>>N;
 
-    vector<string>temp(N, ".");
-    vector<bool>rowsVis(N, false);
-    vector<bool>colsVis(N, false);
     vector<vector<int>>dir;
     dir = {
             {1,2}, {1,-2}, {-1, 2}, {-1,-2},
             {2,1}, {2,-1}, {-2, 1}, {-2,-1}
           };
-    vector<vector<string>>visited(N, vector<string>(N,"0"));
-    nQueens(visited, dir, temp, 0, 0, N);
-
-
+    
+    for(int row=0; row<N; row++){
+        for(int col=0; col<N; col++){
+            vector<vector<string>>visited(N, vector<string>(N,"-1"));
+            bool ans = nQueens(visited, dir, 0, row, col, N);
+            if(ans){
+                display(visited);
+            }            
+        }
+    }
     return 0;
 }
