35659ee8d92a0e02280805346082ac325fe0baab
Voyager1221 <aayushbhadani111@gmail.com>
2024-02-22 20:11:54 +0530

    Implemented a solution, but the testing is left. To be continued tomorrow.

--- MyCodes/DSA/Recursion/RecursionRevision/Sudoku.cpp
+++ MyCodes/DSA/Recursion/RecursionRevision/Sudoku.cpp
@@ -4,18 +4,43 @@
 
 using namespace std;
 
-void sudokuSolver(vector<vector<string>>&sudokuMatrix, vector<vector<int>>&rowHashMap, vector<vector<int>>&colHashMap, int x, int y, int N){
-    
-    // int row = 3*(x/3)+ (y/3);
-    // int col = 3*(x%3) + (y%3);
+// void insertPossibleNumber(vector<vector<string>>&sudokuMatrix, vector<vector<int>>&rowHashMap, vector<vector<int>>&colHashMap, vector<vector<int>>&subMatrixHashMap, int p, int q, int x, int N){
+//     int r = 2*x + (p+q);
+//     for(int num=1; num<=9; num++){
+//         if(rowHashMap[p][num]==0 && colHashMap[q][num] ==0 && subMatrixHashMap[r][num]==0){
+//             sudokuMatrix[p][q] = num;
+//         }
+//     }
+// }
 
-    for(int )
+void sudokuSolver(vector<vector<string>>&sudokuMatrix, vector<vector<int>>&rowHashMap, vector<vector<int>>&colHashMap, vector<vector<int>>&subMatrixHashMap, int x, int cnt, int N){
+    
+
+    for(int y=0; y<N; y++){
+        int p = 3*(x/3)+ (y/3);
+        int q = 3*(x%3) + (y%3);
+        cnt++;
+        if(sudokuMatrix[p][q]=="."){
+            x = cnt/9;
+            // insertPossibleNumber();
+            int r = 2*x + (p+q);
+            for(int num=1; num<=9; num++){
+                if(rowHashMap[p][num]==0 && colHashMap[q][num] ==0 && subMatrixHashMap[r][num]==0){
+                    sudokuMatrix[p][q] = to_string(num);
+                    sudokuSolver(sudokuMatrix, rowHashMap, colHashMap, subMatrixHashMap, x, cnt, N);
+                    sudokuMatrix[p][q] = ".";
+                }
+            }
+
+        }
+    }
 }
 
 int main(){
 
     vector<vector<int>>rowHashMap(9, vector<int>(10, 0));
     vector<vector<int>>colHashMap(9, vector<int>(10, 0));
+    vector<vector<int>>subMatrixHashMap(9, vector<int>(10,0));
     vector<vector<string>>sudokuMatrix(9, vector<string>(9, ""));
 
     int N = 9;
@@ -25,12 +50,20 @@ int main(){
             cin>>s;
             sudokuMatrix[row][col] = s;
             if(s!="."){
-                rowHashMap[row][stoi(s)] = 1;
-                colHashMap[col][stoi(s)] = 1;
+                int num = stoi(s);
+                rowHashMap[row][num] = 1;
+                colHashMap[col][num] = 1;
+
+                int p = row/3;
+                int q = col/3;
+                // r represents the o based indexing of sub-matrices ranging 0-8, wiz [0-1-2][3-4-5][6-7-8]. Each index is a sub-matrix in itself.
+                int r = (row/3)*2 + p+q;
+                // int r = row + q; but this will not give us the required ans, because we are storing value in int data type hence missing the fractional values.
+
+                subMatrixHashMap[r][num] = 1;
             }
         }
     }
-
     // for(int i=0; i<N; i++){
     //     int cnt = 0;
     //     for(int j=0; j<N; j++){
@@ -42,5 +75,18 @@ int main(){
     //     }
     //     cout<<"\n"<<endl;
     // }
+
+    for(int row=0; row<N; row++){
+        int cnt = 0;
+        for(int col=0; col<N; col++){
+            int p = row/3;
+            int q = col/3; 
+            int r = (row/3)*2 + p+q;    
+            cout<<"("<<p<<", "<<q<<", "<<row<<") ";
+
+        }
+        cout<<"\n"<<endl;
+    }
+
     return 0;
 }
